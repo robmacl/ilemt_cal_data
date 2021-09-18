2ca12346f0e2898ef83d0a309b0a1c4b3515b867
Rob MacLachlan <ram@ri.cmu.edu>
2021-09-18 17:07:02 -0400

    Configuration for cal_9_10_premo_cmu

---
+++ cal_9_15_premo_cmu/local_cal_options.m
@@ -0,0 +1,23 @@
+options.source = 'CMU';
+options.sensor = 'premo';
+
+%{
+soYoutZup_seZinXdown_sweep.dat
+soYoutZup_seZinYup_sweep.dat
+soYoutZup_seYoutZup_sweep.dat
+soXinYup_seYoutZup_md.dat
+soXinYup_seYoutZup_ld.dat
+soZoutYup_seYoutZup_ld.dat
+soZoutYup_seYoutZup_md.dat
+soXoutZup_seYoutZup_md.dat
+%}
+
+% Got the source signs messed up in some of this data, probably from doing
+% "Ref cal" in the wrong source fixture.
+options.source_signs = {
+    'soZoutYup_seYoutZup_(md|ld).dat' [-1 -1 1]
+    'soXoutZup_seYoutZup_md.dat' [-1 -1 1]
+    };
+
+    
+    
---
+++ cal_9_15_premo_cmu/local_check_options.m
@@ -0,0 +1,2 @@
+options.concentric_cal_file = 'XYZ_all_concentric_hr_cal';
+%options.valid_threshold = Inf;
